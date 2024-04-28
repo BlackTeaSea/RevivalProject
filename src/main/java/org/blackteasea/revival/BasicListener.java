17317a84215341faaa62cbc307ea9601c08c968b
jellyfishsh <hikurage@protonmail.com>
2024-04-27 21:48:17 -0700

    Added a broadcast message to BasicListener Added a component message to BasicListener

--- src/main/java/org/blackteasea/revival/BasicListener.java
+++ src/main/java/org/blackteasea/revival/BasicListener.java
@@ -1,19 +1,24 @@
 package org.blackteasea.revival;
 
-import org.bukkit.command.BufferedCommandSender;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class BasicListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onDeath(PlayerDeathEvent event) {
+        //Get name of the player
         String player = event.getPlayer().getName();
+
+        //Says the name of the player, and "had to die" in console
         Data.getInstance().getJavaPlugin().getLogger().info(player + " had to die!");
 
+        final Component message = Component.text("OHHHHH NOOOOOOOO! " + player + " seems to have had his mortal string cut. How unfortunate.");
+        Data.getInstance().getJavaPlugin().getServer().broadcast(message);
     }
 }
--- src/main/java/org/blackteasea/revival/Data.java
+++ src/main/java/org/blackteasea/revival/Data.java
@@ -43,4 +43,6 @@ public class Data extends PropertyChangeSupport {
     public List<Player> getPlayerList(){
         return this.playerList;
     }
+
+
 }
