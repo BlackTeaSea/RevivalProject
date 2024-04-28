274a6ff297021020a55b1a9bac4989d7dee66f06
Jonathan <jara.jonathan.4000@gmail.com>
2024-04-28 01:47:24 -0700

    Gamerule thing added

--- src/main/java/org/blackteasea/revival/Revival.java
+++ src/main/java/org/blackteasea/revival/Revival.java
@@ -1,6 +1,7 @@
 package org.blackteasea.revival;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Revival extends JavaPlugin {
@@ -12,10 +13,13 @@ public final class Revival extends JavaPlugin {
         getLogger().info("Starting The Revival Project");
         getServer().getPluginManager().registerEvents(new Death(), this);
         getServer().getPluginManager().registerEvents(new Resurrect(), this);
+        getServer().getWorlds().forEach(world -> world.setGameRuleValue("sendCommandFeedback", "false"));
+        getLogger().info("Command feedback is now off");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getWorlds().forEach(world -> world.setGameRuleValue("sendCommandFeedback", "true"));
     }
 }
