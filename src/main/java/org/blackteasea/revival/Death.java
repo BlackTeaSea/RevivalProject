3703b59c091ca29017c2a717adcc795ee62b743b
jellyfishsh <hikurage@protonmail.com>
2024-04-28 02:42:09 -0700

    funny stuff

--- src/main/java/org/blackteasea/revival/Death.java
+++ src/main/java/org/blackteasea/revival/Death.java
@@ -1,6 +1,7 @@
 package org.blackteasea.revival;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,8 +10,10 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public class Death implements Listener {
     @EventHandler
     public void uponDeath(PlayerDeathEvent event) {
+
         Data data = Data.getInstance();
         Player player = event.getPlayer();
+        player.setGameMode(GameMode.SPECTATOR);
         data.getJavaPlugin().getLogger().info(player.getName() + " has died and will come back!");
         if (!data.getPlayerList().contains(player)) {
             data.addPlayer(player);
