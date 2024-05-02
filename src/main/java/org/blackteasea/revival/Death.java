221aa0eed2403a51c361ed7d17a3a7cce312510b
Jonathan <jara.jonathan.4000@gmail.com>
2024-05-01 22:07:12 -0700

    worked on newSpawnLocation

--- src/main/java/org/blackteasea/revival/Death.java
+++ src/main/java/org/blackteasea/revival/Death.java
@@ -1,13 +1,10 @@
 package org.blackteasea.revival;
 
 import org.bukkit.GameMode;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-
-import static org.blackteasea.revival.Logic.newSpawnLocation;
 
 public class Death implements Listener {
     @EventHandler
@@ -15,13 +12,11 @@ public class Death implements Listener {
 
         Data data = Data.getInstance();
         Player player = event.getPlayer();
-        player.setGameMode(GameMode.SPECTATOR);
+//        player.setGameMode(GameMode.SPECTATOR);
         data.getJavaPlugin().getLogger().info(player.getName() + " has died and will come back!");
         if (!data.getPlayerList().contains(player)) {
             data.addPlayer(player);
         }
-        Location location = player.getLocation();
-        player.setRespawnLocation(newSpawnLocation(location));
     }
 
 
--- src/main/java/org/blackteasea/revival/Resurrect.java
+++ src/main/java/org/blackteasea/revival/Resurrect.java
@@ -6,6 +6,7 @@ import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.HumanEntity;
@@ -18,6 +19,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.blackteasea.revival.Logic.newSpawnLocation;
 
 public class Resurrect implements Listener {
 
@@ -55,6 +58,11 @@ public class Resurrect implements Listener {
         List<Player> playerListCopy = new ArrayList<>(Data.getInstance().getPlayerList());
         for (Player player : playerListCopy) {
             if (player.getName().equals(resurrected)) {
+                Location location = player.getLastDeathLocation();
+                assert location != null;
+                Location newLoc = newSpawnLocation(location);
+                Data.getInstance().getJavaPlugin().getLogger().info("New location: " + newLoc);
+                player.teleport(newLoc);
                 player.setGameMode(org.bukkit.GameMode.SURVIVAL);
                 Data.getInstance().removePlayer(player);
                 inv.remove(clickedItem);
