a427adfa43d0a1e0bc2c629d22a3f5122b5a8aac
jellyfishsh <hikurage@protonmail.com>
2024-05-04 15:20:20 -0700

    Added advancement status to revive menu

--- src/main/java/org/blackteasea/revival/Cost.java
+++ src/main/java/org/blackteasea/revival/Cost.java
@@ -9,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Statistic;
 import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -20,80 +21,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cost {
-    public static String[] advancementChecks = {
-            "story/mine_stone",
-            "story/smelt_iron",
-            "story/iron_tools",
-            "story/form_obsidian",
-            "story/mine_diamond",
-            "story/enter_the_nether",
-            "story/shiny_gear",
-            "story/enter_the_end",
-            "nether/obtain_blaze_rod",
-            "end/dragon_breath",
-            "end/kill_dragon",
-            "end/find_end_city",
-            "adventure/kill_a_mob",
-            "adventure/kill_all_mobs"
+    public static String[][] advancementChecks = {
+            {"story/mine_stone", "Stone Breaker"},
+            {"story/smelt_iron", "Iron Smelter"},
+            {"story/mine_diamond", "Gem Hoarder"},
+            {"story/enter_the_nether", "Hell Diver"},
+            {"story/enter_the_end", "Witness to the End"},
+            {"end/kill_dragon", "Dragonslayer"},
+            {"adventure/kill_a_mob", "Hunter"},
+            {"adventure/trade", "Capitalist"},
+            {"adventure/totem_of_undying", "Reincarnated"},
+
             };
 
 
     public static int getDeathTime(Player player){return player.getStatistic(Statistic.TIME_SINCE_DEATH)/20;}
     public static int getDeathCount(Player player) {return player.getStatistic(Statistic.DEATHS);}
 
+    public static AdvancementProgress getAdvancementProgress(Player player, String plugin, String advancementName){
+        NamespacedKey namespace = new NamespacedKey(plugin, advancementName);
+        Advancement advancement = Data.getInstance().getJavaPlugin().getServer().getAdvancement(namespace);
+        return player.getAdvancementProgress(advancement);
+    }
+
+    public static boolean hasCompleted(Player player, String advancementName){
+        AdvancementProgress toCheck = getAdvancementProgress(player, "minecraft", advancementName);
+        if (toCheck.isDone()){
+            return true;
+        }
+
+        return false;
+    }
+
     public static List<Component> StatComponent (Player player) {
         ArrayList<Component> stats = new ArrayList<>();
+        //Statistics
         stats.add(Component.text(getDeathCount(player) + " Deaths").color(TextColor.color(0xFFFFFF)));
         stats.add(Component.text("Died " + getDeathTime(player) + " seconds ago").color(TextColor.color(0xFFFFFF)));
 
-        return stats;
-    }
-
-    public static boolean checkRequirements(Player user, Player dead){
-        for(ItemStack itemStack : Cost.deathCost(dead)) {
-            if(!user.getInventory().containsAtLeast(itemStack, getDeathCount(dead))){
-
-
-                return false;
+        //Advancements
+        for(String[] titles : advancementChecks){
+            if(hasCompleted(player, titles[0])){
+                stats.add(Component.text(titles[1]).color(TextColor.color(0xFFFDD0)));
             }
         }
 
-        return true;
+        return stats;
     }
-
-    public static List<Component> CostComponent (Player player) {
-
-        //cross-check the
-
-        ArrayList<Component> costs = new ArrayList<>();
-        return costs;
-    }
-
-    public static int test (){
-
-
-        return 0;
-    }
-
-
-
-
-
-
-
-    //TODO: Get item based on achievements
-
-    public static List<ItemStack> deathCost (Player player) {
-
-        for (String advancement : advancementChecks){
-
-        }
-        List<ItemStack> cost = new ArrayList<ItemStack>();
-
-
-        return cost;
-    }
-
 
 
 
