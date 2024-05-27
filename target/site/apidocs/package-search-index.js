9e6037f868c59f431cdd180e11990c1540736e7c
Jonathan Jara <39594511+GoldfishJonny@users.noreply.github.com>
2024-05-27 15:35:07 -0700

    Delete target directory

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.blackteasea.revival"},{"l":"org.blackteasea.revival.Events"},{"l":"org.blackteasea.revival.functions"},{"l":"org.blackteasea.revival.GUIs"},{"l":"org.blackteasea.revival.Items"},{"l":"org.blackteasea.revival.Listeners"}];updateSearchResults();
